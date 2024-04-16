e1e9a1566234a5525a586908504eeed894073310
julia <j.debray@argsolutions.fr>
2024-04-16 13:27:32 +0200

    fix divers

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,85 +4,88 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
+  let $window = $(window),
+    $body = $("body");
 
-	let	$window = $(window),
-		$body = $('body');
+  // Breakpoints.
+  breakpoints({
+    xlarge: ["1141px", "1680px"],
+    large: ["981px", "1140px"],
+    medium: ["737px", "980px"],
+    small: ["481px", "736px"],
+    xsmall: ["321px", "480px"],
+    xxsmall: [null, "320px"],
+  });
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1141px',  '1680px' ],
-			large:    [ '981px',   '1140px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '321px',   '480px'  ],
-			xxsmall:  [ null,      '320px'  ]
-		});
+  function toggleBurgerMenu() {
+    // Récupérer la position du haut de la fenêtre et la hauteur de la fenêtre
+    let windowTopPosition = $(window).scrollTop();
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+    // Récupérer la position de la section "header"
+    let headerPosition = $("#header").offset().top;
+    let headerHeight = $("#header").outerHeight();
 
-	// Scrolly.
-    $('.scrolly').scrolly();
+    // Calculer la position du bas de la section "header"
+    let headerBottomPosition = headerPosition + headerHeight - 1;
 
-    $(document).ready(function() {
-        $(window).scroll(function() {
-            // Récupérer la position du haut de la fenêtre et la hauteur de la fenêtre
-            let windowTopPosition = $(window).scrollTop();
+    // Vérifier si la section "header" est visible à l'écran
+    if (windowTopPosition > headerBottomPosition) {
+      // Si la section "header" n'est pas visible, afficher le menu burger
+      $("#burger-wrapper").fadeIn();
+      $("#burger-icon").css("opacity", "1");
+      $("#burger-icon").css("pointer-events", "auto");
+    } else {
+      // Sinon, cacher le menu burger
+      $("#burger-wrapper").fadeOut();
+      if ($("#burger-menu").css("opacity") === "1") {
+        $("#burger-icon").click();
+        $("#burger-icon").css("pointer-events", "none");
+      }
+    }
+  }
 
-            // Récupérer la position de la section "header"
-            let headerPosition = $('#header').offset().top;
-            let headerHeight = $('#header').outerHeight();
+  // Play initial animations on page load.
+  $window.on("load", function () {
+    window.setTimeout(function () {
+      $body.removeClass("is-preload");
+    }, 100);
+  });
 
-            // Calculer la position du bas de la section "header"
-            let headerBottomPosition = headerPosition + headerHeight;
+  // Scrolly.
+  $(".scrolly").scrolly();
 
-            // Vérifier si la section "header" est visible à l'écran
-            if (windowTopPosition > headerBottomPosition) {
-                // Si la section "header" n'est pas visible, afficher le menu burger
-                $('#burger-wrapper').fadeIn();
-                    $('#burger-icon').css('opacity','1');
-                    $('#burger-icon').css('pointer-events','auto');
+  $(document).ready(function () {
+    toggleBurgerMenu();
+    $(window).scroll(function () {
+      toggleBurgerMenu();
+    });
 
-            } else {
-                // Sinon, cacher le menu burger
-                $('#burger-wrapper').fadeOut();
-                if($('#burger-menu').css('opacity') === '1') {
-                    $('#burger-icon').click();
-                    $('#burger-icon').css('pointer-events','none');
-                }
-            }
-        });
+    $("#burger-icon").on("click", function () {
+      if ($("#burger-menu").css("opacity") === "0") {
+        $("#burger-wrapper").css("background-color", "#333");
+        $("#burger-wrapper").css("pointer-events", "auto");
+        setTimeout(function () {
+          $("#burger-menu").css("opacity", "1");
+        }, 200);
+      } else {
+        $("#burger-wrapper").css("background-color", "transparent");
+        $("#burger-wrapper").css("pointer-events", "none");
+        $("#burger-menu").css("opacity", "0");
+      }
+    });
 
-        $('#burger-icon').on("click", function(){
-            if($('#burger-menu').css('opacity') === '0'){
-                $('#burger-wrapper').css('background-color', '#333');
-                $('#burger-wrapper').css('pointer-events','auto');
-                setTimeout(function() {
-                    $('#burger-menu').css('opacity', '1');
-                }, 200);
-            } else {
-                $('#burger-wrapper').css('background-color', 'transparent');
-                $('#burger-wrapper').css('pointer-events','none');
-                $('#burger-menu').css('opacity','0');
-            }
-        });
-
-        $(document).mouseup(function(e) {
-            const aside = $("#burger-wrapper");
-            if (aside.css('background-color') !== 'transparent' && $("#burger-menu").css("opacity") === "1") {
-            // Si la cible du clic n'est pas l'aside et n'est pas un descendant de l'aside, alors on ferme l'aside
-                if (!aside.is(e.target) && aside.has(e.target).length === 0) {
-                    $('#burger-icon').click();
-                }
-            }
-        });
-
-
-});
-
+    $(document).mouseup(function (e) {
+      const aside = $("#burger-wrapper");
+      if (
+        aside.css("background-color") !== "transparent" &&
+        $("#burger-menu").css("opacity") === "1"
+      ) {
+        // Si la cible du clic n'est pas l'aside et n'est pas un descendant de l'aside, alors on ferme l'aside
+        if (!aside.is(e.target) && aside.has(e.target).length === 0) {
+          $("#burger-icon").click();
+        }
+      }
+    });
+  });
 })(jQuery);
